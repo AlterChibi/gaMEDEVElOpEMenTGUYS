7d8fe28e88f355ce4f8108687013434c80f2db7b
jinxs2011 <38091907+jinxs2011@users.noreply.github.com>
2018-04-16 14:35:43 +1000

    Add files via upload

---
+++ Assets/Scripts/QuadTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuadTest : MonoBehaviour {
+
+    private float collide = 0;
+
+    private void Start()
+    {
+        
+    }
+    private void Update()
+    {
+        
+    }
+    private void OnGUI()
+    {
+        if (collide==2) {
+            GUI.Label(new Rect(750, 300, 100, 20), "sample text");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collide = 1;
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        collide = 2;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+
+        collide = 3;
+    }
+}
